ce1f75c05478d7cf517cccaee559488ec8c91bb8
Juan Sepulveda <juan@lavaina.com>
2015-04-08 18:13:24 -0400

    Add sauce configuration

--- tests/conf.js
+++ tests/conf.js
@@ -1,4 +1,14 @@
 // conf.js
-exports.config = {
+var params = require("rc")("lv"); // parse arguments into a object
+
+var config = {
   specs: ['search/*spec.js']
+};
+
+// Add sauce credentials if we send the sauce flag
+if(params.sauce){
+  config.sauceUser = "spacejamio";
+  config.sauceKey = "86926832-e8b8-4402-a562-f4a6dc2a3049";
 }
+
+exports.config = config;

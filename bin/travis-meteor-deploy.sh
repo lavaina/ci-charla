b561ad935db2949b02a687cb0dae077f58cb1116
Ronen Babayoff <ronen@lavaina.com>
2015-04-08 03:22:24 -0400

    Deploy to ci-lavaina before functional tests

--- bin/travis-meteor-deploy.sh
+++ bin/travis-meteor-deploy.sh
@@ -5,6 +5,7 @@ if [[ "$TRAVIS_BRANCH" == "master" && "$TRAVIS_PULL_REQUEST" == "false" ]]; then
   meteor deploy stage-lavaina
 fi
 
+# This only works in private repos. Public repos need a deploy key
 TAG_NAME="build/$TRAVIS_BUILD_NUMBER"
 git tag "$TAG_NAME"
 git push origin $TAG_NAME
--- bin/travis-test.sh
+++ bin/travis-test.sh
@@ -3,4 +3,5 @@
 cd $TRAVIS_BUILD_DIR/todos
 spacejam test-packages packages/common
 
+meteor deploy ci-lavaina
 # TODO: Juan - run functional tests
